2be0f63eed7b80ed9a9ea142c3e4456f9d4f07d2
Nathaniel Coleman <satorugojo8868@outlook.com>
2026-03-14 21:38:16 -0400

    implement rest data provider

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -37,7 +37,7 @@ const SubjectsList = () => {
             filterFn:'includesString'
           },
           {id: 'department',
-            accessorKey: 'department',
+            accessorKey: 'department.name',
             size:150,
             header: () => <p className="column-title">Department</p>,
             cell: ({getValue}) => <Badge variant="secondary">{getValue<string>()}</Badge>
