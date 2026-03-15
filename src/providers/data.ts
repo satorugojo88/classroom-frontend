ada22cf2744049de72e506aaade8e076b7113d1c
Nathaniel Coleman <satorugojo8868@outlook.com>
2026-03-14 21:56:22 -0400

    implement code rabbit suggestions

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -27,13 +27,13 @@ const options: CreateDataProviderOptions = {
 
     mapResponse: async (response) =>
     {
-      const payload: ListResponse = await response.json()
+      const payload: ListResponse = await response.clone().json()
 
       return payload.data ?? []
     },
 
     getTotalCount: async (response) => {
-      const payload: ListResponse = await response.json()
+      const payload: ListResponse = await response.clone().json()
 
       return payload.pagination?.total ?? payload.data?.length ?? 0
     }
